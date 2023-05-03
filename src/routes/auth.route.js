b18572542ae0fff8e85232ef216ec8bdc8a7bbe4
makarov009 <prottoy.r.ahmed@gmail.com>
2023-05-03 18:33:42 +0600

    auth: validation initiated by express-validatior

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authController = require('../controllers/auth.controller')
+const authController = require("../controllers/auth.controller");
+const authValidator = require("../validators/auth.validator");
 
-router.post("/sign-up",authController.signUp);
-router.post("/sign-in",authController.signIn);
-router.post("/sign-out",authController.signOut);
+router.post("/sign-up", authController.signUp);
+router.post("/sign-in", authController.signIn);
+router.post("/sign-out", authController.signOut);
 
 module.exports = router;
