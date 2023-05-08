ddd1fa8c699545bd192527acdb756faa4b3b081b
prottoyAhmed <prottoy.r.ahmed@gmail.com>
2023-05-08 21:48:56 +0600

    user: user update functionality added.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,7 @@
 const StatusCode = require("../utils/Objects/StatusCode");
 const AppError = require("../utils/AppError");
-const { getUserById } = require("../repositories/user.repo");
+const { getUserById, updateUser } = require("../repositories/user.repo");
+const authUtil = require("../utils/auth.util");
 
 exports.user = async (userId) => {
   const user = await getUserById(userId);
@@ -10,4 +11,20 @@ exports.user = async (userId) => {
   }
 
   return user;
+};
+
+exports.updateUser = async (userId, updateBody, password) => {
+  const user = await getUserById(userId);
+
+  if (!user) {
+    throw new AppError(StatusCode.NOT_FOUND, "User not found");
+  }
+
+  if (!(await authUtil.comparePassword(password, user.password))) {
+    throw new AppError(StatusCode.BAD_REQUEST, "Password is incorrect");
+  }
+
+  await updateUser(userId, updateBody);
+
+  return await getUserById(userId);
 };
