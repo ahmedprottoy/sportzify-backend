58cc378f7c00bd3af18cc4105d9f4671b0b0cf4e
makarov009 <prottoy.r.ahmed@gmail.com>
2023-05-11 18:11:38 +0600

    file: limiting file size and filtering type

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -1,6 +1,23 @@
 const multer = require("multer");
+const AppError = require("../utils/AppError");
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+
+  fileFilter: (req, file, callback) => {
+    if (
+      file.mimetype.startsWith("image/") &&
+      /\.(jpg|jpeg|png)$/.test(file.originalname)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new AppError(400, "Only JPEG and PNG file formats are allowed"));
+    }
+  },
+});
 
 module.exports = upload;
