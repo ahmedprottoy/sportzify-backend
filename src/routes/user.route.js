2a6bac9f29228301dc80ec160abcfe81056916ff
prottoyAhmed <prottoy.r.ahmed@gmail.com>
2023-05-14 23:55:18 +0600

    fixed user route jsdoc

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,6 +7,24 @@ const fileUpload = require("../middlewares/fileUpload.middleware");
 const upload = require("../config/multer.config");
 const userRouter = express.Router();
 
+/**
+ * Express router for blog routes
+ * @module userRoutes
+ */
+
+
+/**
+ * Route for getting user's info, updating user's profile and delete a user.
+ * @name /:username
+ * @function
+ * @memberof module:userRoutes
+ * @param {string} path - Express route path
+ * @param {Function} controller - Express controller function
+ * @param {Function} middleware - Express middleware function
+ * @param {Function} validation - Express validation middleware function
+ */
+
+
 userRouter
   .route("/:username")
   .get(userController.user)
@@ -18,6 +36,17 @@ userRouter
   )
   .delete(checkToken, userController.deleteUser);
 
+
+/**
+ * Route for getting user's password, updating user's password.
+ * @name /password/:username
+ * @function
+ * @memberof module:userRoutes
+ * @param {string} path - Express route path
+ * @param {Function} controller - Express controller function
+ * @param {Function} middleware - Express middleware function
+ * @param {Function} validation - Express validation middleware function
+ */
 userRouter
   .route("/password/:username")
   .put(
@@ -27,8 +56,32 @@ userRouter
     userController.passwordUpdate
   );
 
+
+/**
+ * Route for getting user's blogs.
+ * @name /blogs/:username
+ * @function
+ * @memberof module:userRoutes
+ * @param {string} path - Express route path
+ * @param {Function} controller - Express controller function
+ * @param {Function} middleware - Express middleware function
+ * @param {Function} validation - Express validation middleware function
+ */
+ 
 userRouter.route("/blogs/:username").get(userController.allBlogs);
 
+
+/**
+ * Route for getting user's image, updating user's image and delete a user's image.
+ * @name /image/:username
+ * @function
+ * @memberof module:userRoutes
+ * @param {string} path - Express route path
+ * @param {Function} controller - Express controller function
+ * @param {Function} middleware - Express middleware function
+ * @param {Function} validation - Express validation middleware function
+ * @param {Function} uploadMiddleware - Express upload middleware function
+ */
 userRouter
   .route("/image/:username")
   .post(
